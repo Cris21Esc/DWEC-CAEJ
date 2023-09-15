c56a218d976fb39dc93083d9f455ae102fee2ab4
Cristopher Escalante <cristopheralex2001@hotmail.com>
2023-09-15 10:15:38 +0200

    Tercer Commit

--- subir.sh
+++ subir.sh
@@ -8,9 +8,10 @@ echo "Paso 2: Añadimos archivos locales"
 git add .
 
 echo "Paso 3: Descripción de la subida"
-read -p "Escibe el mensaje de subida" mensaje
+read -p "Escribe el mensaje de subida: " mensaje
 git commit -m "$mensaje" .
 
 echo "Paso 4: Realizar subida"
+echo "Recuerda tu token: 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"
 git push
 
