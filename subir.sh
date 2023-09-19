3ac7c79351bbc6cd871801b93852dd6581da2fa5
Cristopher Escalante <cristopheralex2001@hotmail.com>
2023-09-19 10:34:47 +0200

    prueba

--- subir.sh
+++ subir.sh
@@ -12,6 +12,6 @@ read -p "Escribe el mensaje de subida: " mensaje
 git commit -m "$mensaje" .
 
 echo "Paso 4: Realizar subida"
-echo "Recuerda tu token: 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"
+echo "Recuerda tu token: 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_"
 git push
 
