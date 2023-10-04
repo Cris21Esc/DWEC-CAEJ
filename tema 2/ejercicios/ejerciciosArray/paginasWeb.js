608b6654878923c457bc2a5a3deb5b53f46a3979
Cristopher Escalante <cristopheralex2001@hotmail.com>
2023-10-04 12:37:16 +0200

    ej 4.2

---
+++ tema 2/ejercicios/ejerciciosArray/paginasWeb.js
@@ -0,0 +1,74 @@
+
+ /*EJ1*/ 
+/*let saludar =()=>{alert("Hola, te estoy vigilando")};
+let idInterv=setInterval(saludar,7000);/
+
+/*EJ2*/
+/*let cuenta =10.00;
+let descontar=()=>{
+    if(cuenta>=0){
+        console.log(cuenta.toFixed(2));
+        cuenta-=0.01;
+    }
+    else{
+        alert("Te ha llegado la hora");
+    }
+}
+let idTimeout=setInterval(descontar,10);*/
+
+/*EJ3*/
+/*let fecha =()=>{
+    let fechahoy = new Date();
+    console.log(`${fechahoy.getHours()}:${fechahoy.getMinutes()}:${fechahoy.getSeconds()}`);
+}
+let mostrar = setInterval(fecha,1000);*/
+
+/*EJ4*/
+
+/*let fecha =()=>{
+    let fecha2 = new Date('2023/09/30');
+    let fecha1 = new Date()
+    
+    let resta = fecha2.getTime() - fecha1.getTime()
+    console.log(`Dias: ${Math.floor(resta/(1000*60*60*24))}, Horas: ${Math.floor(resta/(1000*60*60))}, Minutos: ${Math.floor(resta/(1000*60))}, Segundos: ${Math.floor(resta/(1000))}`)
+}
+let mostrar = setInterval(fecha,1000);*/
+
+/*EJ5*/
+/*let socios = [];
+do{
+    socios=prompt("Especifica los socios").split(",");
+
+}while(socios.length<3);
+
+console.log(`numero de socios: ${socios.length}`);
+
+console.log(`socios por orden: ${socios.sort().join("<-*->")}`);
+
+let JDH =()=>{
+    socios.pop();
+    console.log(`socios por orden: ${socios.sort().join("<-*->")}`);
+}
+while(socios.length>=0){let mostrar = setInterval(JDH,1000);}*/
+
+/*Ejercicio 4.2*/
+$numAlum = parseInt(prompt("¿cuantos alumnos vas a adjuntar?"));
+let clase =[];
+let media = 0.0;
+function Alumno(nombre,nota){
+    this.nombre = nombre;
+    this.nota = nota;
+}
+for(let i = 0; i<$numAlum;i++){
+    let nombre = prompt("Dime el nombre del alumno");
+    let nota = parseFloat(prompt("Dime la nota del alumno"));
+    let nuevoAlumno = new Alumno(nombre, nota);
+    clase.push(nuevoAlumno);
+    media += nuevoAlumno.nota;
+}
+
+clase.forEach(alumno => {
+    console.table(alumno)
+});
+console.log(`La media es ${media/parseFloat(clase.length)}`)
+
