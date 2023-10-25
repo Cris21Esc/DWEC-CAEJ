dbdc2bc92b400715c91d5ef977d6cd4750b14e16
Cristopher Escalante <cristopheralex2001@hotmail.com>
2023-10-25 11:47:41 +0200

    aniadido lista

---
+++ tema 4/DOM/dom.js
@@ -0,0 +1,11 @@
+/*document.forms[0];
+document.getElementsByName("sexo");
+document.querySelectorAll("input[name='sexo']");
+document.firstElementChild;*/
+
+
+//  DOM
+
+let h1 = document.createElement("h1");
+h1.innerHTML = "Hola Caracola";
+document.body.appendChild(h1);
---
+++ tema 4/DOM/ejemploDom/aniadirJS.js
@@ -0,0 +1,66 @@
+/*function anadirElemento(){
+
+    let contenido = document.getElementById("contenido").value;
+    if(contenido != ""){
+        // Elemento creado
+    let elemento = document.createElement("li");
+
+    // Especificar contenido del elemento    
+    elemento.innerHTML = `${contenido}`;
+
+    // Le damos estilos
+    elemento.classList.add("rojo")
+
+    // Damos atributos extra
+    elemento.setAttribute("title", contenido);
+    elemento.setAttribute("id",`id${contenido}`)
+
+    // Especificar eventos
+        elemento.addEventListener("click",function(){
+            this.remove();
+        })
+
+
+    // Recuperar el padre
+    let padre = document.getElementById("lista");
+
+    // Añado al padre el hijo
+    padre.appendChild(elemento);
+    }else{
+        alert("Especifica un contenido")
+    }
+}*/
+
+function anadirElemento(){
+
+    //let contenido = document.getElementById("contenido").value;
+    let elemento = document.createElement("li");
+
+    // Especificar contenido del elemento    
+    elemento.innerHTML = `${Math.random()}`;
+
+    // Le damos estilos
+    elemento.classList.add("rojo")
+
+    // Damos atributos extra
+    /*elemento.setAttribute("title", contenido);
+    elemento.setAttribute("id",`id${contenido}`);*/
+
+    // Especificar eventos
+    elemento.addEventListener("click",function(){
+         this.remove();
+    });
+
+
+    // Recuperar el padre
+    let padre = document.getElementById("lista");
+
+    // Añado al padre el hijo
+    padre.appendChild(elemento);
+}
+
+function limpiarLista(){
+    let elementos = document.querySelectorAll("ul#lista li");
+    elementos.forEach(element=>{element.remove()});
+
+}
