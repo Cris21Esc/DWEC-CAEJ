0749c5a7b310c350890614fd488ee8446aa96a5d
Cristopher Escalante <cristopheralex2001@hotmail.com>
2024-01-31 08:55:05 +0100

    nuse

--- vuejs/CosoPaginas/src/paginas/paginaPersonal.vue
+++ vuejs/CosoPaginas/src/paginas/paginaPersonal.vue
@@ -3,6 +3,7 @@ import servicioAficiones from "../servicios/personal/servicioAficiones"
 import { onMounted, ref } from "vue";
 
     let aficiones = ref(null);
+    let imagenCambio = ref("https://images.unsplash.com/photo-1595433707802-6b2626ef1c91?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D")
 
     function obtenerAficiones(){
         servicioAficiones
@@ -17,14 +18,19 @@ import { onMounted, ref } from "vue";
     }
 
     onMounted(()=> obtenerAficiones());
+
+    function funcionAficion(aficion){
+        console.log(aficion.nombre);
+        imagenCambio.value = aficion.url;
+    }
 </script>
 <template>
     <div>
         <ul>
             <li v-for="(aficion,id) in aficiones" :key="id">
-                {{ aficion.nombre }}
-                <img :src="aficion.url">
+                <span @click="funcionAficion(aficion)">{{ aficion.nombre }}</span>
             </li>
         </ul>
+        <img  :src="imagenCambio">
     </div>
 </template>
