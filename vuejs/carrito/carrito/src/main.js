7e8f75c4f9ce56663df633f085940371a15a30be
Cristopher Escalante <cristopheralex2001@hotmail.com>
2024-02-20 08:53:46 +0100

    aniadidoSweetAlert

--- vuejs/carrito/carrito/src/main.js
+++ vuejs/carrito/carrito/src/main.js
@@ -1,11 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import VueSweetalert2 from 'vue-sweetalert2';
+// If you don't need the styles, do not connect
+import 'sweetalert2/dist/sweetalert2.min.css';
+
 import './assets/main.css'
 import router from './router';
 
 const app = createApp(App);
 
 app.use(router);
-
+app.use(VueSweetalert2);
 app.mount("#app");
